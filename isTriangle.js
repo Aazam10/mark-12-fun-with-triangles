5f418915789113952456de46eed14749d000baeb
Aazam10 <aazam.hudda94@gmail.com>
2021-09-11 16:53:54 +0530

    fix:validation for null values

--- isTriangle.js
+++ isTriangle.js
@@ -8,15 +8,21 @@ function calculateSumOfAngles(angle1,angel2,angel3){
     var sumOfAngles=angle1+angel2+angel3;
     return sumOfAngles;
 }
-
+function hidemessage(){
+    outputRef.innerText='';
+}
 
 function isTriangle(){
-    var sum=calculateSumOfAngles(Number(listOfAngles[0].value),Number(listOfAngles[1].value),Number(listOfAngles[2].value));
+    hidemessage();
+    if(Number(listOfAngles[0].value) && Number(listOfAngles[1].value) && Number(listOfAngles[2].value)){
+         var sum=calculateSumOfAngles(Number(listOfAngles[0].value),Number(listOfAngles[1].value),Number(listOfAngles[2].value));
     if(sum===180){
         outputRef.innerText='Yayy, the angles form a triangle';
     }else{
         outputRef.innerText='Oh Oh!, the angles do not form a triangle';
     }
+    }
+   
 }
 
 
